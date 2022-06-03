bfc62cbd3ad40a7c67a3c1426a4c4e5394e52b64
MateoCoriaChris <106635887+MateoCoriaChris@users.noreply.github.com>
2022-06-02 23:00:02 -0300

    Update practico_uno.py

--- practico_uno.py
+++ practico_uno.py
@@ -38,7 +38,7 @@ else:
             #imprimimos con un "f" str ya que nos da la posibilidad de mostrar datos y 
             # variables de una forma mas prolija... la "\n" es para hacer un salto de linea.
             print(f"De la suma {num1} + {num2} + {num3} el resultado es : {resultado}\n")
-elif opcion == 2:
+    elif opcion == 2:
         try:
             num1 = float(input("Ingresa el primer numero: "))
             num2 = float(input("Ingresa el segundo numero: "))
